78bfff692646ba844979bba7f44783d2fab6e5b7
adarsh-chaunal <adarsh97@gmail.com>
2024-01-28 01:22:31 +0530

    Search service with synchronous communication with auction service

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,25 +1,58 @@
+using System.Net;
+using Microsoft.VisualBasic;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Model;
+using SearchService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+
+// this will trigger the dbInitializer after the application is fully running.
+// otherwise the builder will keep waiting in the try block and application will not run if db not initialized
+app.Lifetime.ApplicationStarted.Register(async () => 
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        
+        System.Console.WriteLine(e);
+    }
+});
+
+// try
+// {
+//     await DbInitializer.InitDb(app);
+// }
+// catch (Exception e)
+// {
+    
+//     System.Console.WriteLine(e);
+// }
+
 app.Run();
+
+// called when search service donot find auction service for the http request to get the data
+// Do something if we get an exception
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
